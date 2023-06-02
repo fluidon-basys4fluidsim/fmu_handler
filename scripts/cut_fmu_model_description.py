38e95f43f14739fad38e2bcbc79efb96ccf5b925
Steinmautz89 <Steinmautz@googlemail.com>
2023-06-02 18:58:43 +0200

    Interrim state

--- scripts/cut_fmu_model_description.py
+++ scripts/cut_fmu_model_description.py
@@ -1,30 +1,20 @@
+import json
+import fnmatch
 from pathlib import Path
 from typing import Union, List, Optional
 from fmu_handler.fmu_adapter import FMUAdapter
-import json
-import io
-import fnmatch
+from fmu_handler.fmu_types import FMUScalarVariable, Causality
+from utils.custom_file_handling import check_create_directory_path
 
 
-def _combine_components_and_params(elements: List):
-    components_params = []
-    for element in elements:
-        for component, parameters in element.items():
-            for parameter in parameters:
-                components_params.append(f'{component}.{parameter}')
-    return components_params
-
-
-def _cut_fmu_model_description_with_positive_list(positive_list: List, negative_list: List, fmu: FMUAdapter)\
+def _cut_fmu_model_description_with_positive_list(keep_list: List, delete_list: List, fmu: FMUAdapter)\
         -> FMUAdapter:
-    positive_elements = _combine_components_and_params(positive_list)
-    negative_elements = _combine_components_and_params(negative_list)
-    for scalar_variable in fmu.model_description.model_variables.scalar_variables:
+    for scalar_variable in fmu.query_scalar_variables(query=FMUScalarVariable(causality=Causality.parameter)):
         delete = False
-        for negative_element in negative_elements:
+        for negative_element in delete_list:
             if fnmatch.fnmatch(scalar_variable.name, negative_element):
                 delete = True
-        for positive_element in positive_elements:
+        for positive_element in keep_list:
             if fnmatch.fnmatch(scalar_variable.name, positive_element):
                 delete = False
         if delete:
@@ -46,15 +36,17 @@ def reduce_fmu_model_descriptions_in_directory(dir_path: Union[str, Path], outpu
         config_data = json.load(config_file)
         config_file.close()
 
-    positive_list = config_data.get("keep_elements", [])
-    negative_list = config_data.get("delete_elements", [])
+    keep_list = config_data.get("keep_elements", [])
+    delete_list = config_data.get("delete_elements", [])
     for fmu_file in dir_path.iterdir():
         if fmu_file.suffix == ".fmu":
             fmu = FMUAdapter(fmu_file=fmu_file)
             fmu = _cut_fmu_model_description_with_positive_list(
-                positive_list=positive_list, negative_list=negative_list, fmu=fmu)
-            fmu.save_fmu_copy(file_name=f"{fmu_file.stem}{output_suffix}", tar_dir_path=dir_path)
+                keep_list=keep_list, delete_list=delete_list, fmu=fmu
+            )
+            save_dir_path = check_create_directory_path(dir_path=dir_path.joinpath("reduced"), create=True)
+            fmu.save_fmu_copy(file_name=f"{fmu_file.stem}{output_suffix}", tar_dir_path=save_dir_path)
 
 
 if __name__ == "__main__":
-    reduce_fmu_model_descriptions_in_directory(dir_path="./")
+    reduce_fmu_model_descriptions_in_directory(dir_path="./", output_suffix=None)
