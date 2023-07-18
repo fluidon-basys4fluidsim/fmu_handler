87741edf71f10fe662fc8645d49ab07e3e56a703
Steinmautz89 <Steinmautz@googlemail.com>
2023-07-19 01:17:28 +0200

    Added option for outut directory for fmu parameter cutting.

--- scripts/cut_fmu_model_description.py
+++ scripts/cut_fmu_model_description.py
@@ -9,6 +9,19 @@ from utils.custom_file_handling import check_create_directory_path
 
 def _cut_fmu_model_description_with_positive_list(keep_list: List, delete_list: List, fmu: FMUAdapter)\
         -> FMUAdapter:
+    """
+    Function to delete defined elements.
+
+    Generally, all elements are selected not to be deleted.
+    First, all elements that fit to the delete_list are tagged to be deleted.
+    Then, elements to be kept might untag those again.
+    Finally, all tagged scalar variables are deleted from the fmu model description xml.
+
+    :param keep_list: Elements that should be kept.
+    :param delete_list: Elements that should be deleted.
+    :param fmu:
+    :return:
+    """
     for scalar_variable in fmu.query_scalar_variables(query=FMUScalarVariable(causality=Causality.parameter)):
         delete = False
         for negative_element in delete_list:
@@ -22,7 +35,20 @@ def _cut_fmu_model_description_with_positive_list(keep_list: List, delete_list: 
     return fmu
 
 
-def reduce_fmu_model_descriptions_in_directory(dir_path: Union[str, Path], output_suffix: Optional[str] = "reduced"):
+def reduce_fmu_model_descriptions_in_directory(dir_path: Union[str, Path],
+                                               output_path: Optional[Union[str, Path]] = None,
+                                               output_suffix: Optional[str] = "reduced"):
+    """
+    According to the parameter_reduction_config.json file in the directory, the model description of all fmus in this
+    directory are reduced.
+
+    :param dir_path: Path to the directory containing the fmus.
+    :param output_path: If None, the reduced fmus are saved and possibly overwritten
+    in the same directory as the original ones.
+    :param output_suffix: If None, the reduced fmus are saved and possibly overwritten. Otherwise, the suffix is added
+    to the modified fmu files.
+    :return:
+    """
     dir_path = Path(dir_path)
     if not dir_path.is_dir():
         raise NotADirectoryError(f"Path {dir_path} is not a directory.")
@@ -32,7 +58,7 @@ def reduce_fmu_model_descriptions_in_directory(dir_path: Union[str, Path], outpu
     elif not output_suffix.startswith("_"):
         output_suffix = f"_{output_suffix}"
 
-    with open("./parameter_reduction_config.json", "r") as config_file:
+    with open(dir_path.joinpath("parameter_reduction_config.json"), "r") as config_file:
         config_data = json.load(config_file)
         config_file.close()
 
@@ -44,9 +70,15 @@ def reduce_fmu_model_descriptions_in_directory(dir_path: Union[str, Path], outpu
             fmu = _cut_fmu_model_description_with_positive_list(
                 keep_list=keep_list, delete_list=delete_list, fmu=fmu
             )
-            save_dir_path = check_create_directory_path(dir_path=dir_path.joinpath("reduced"), create=True)
+
+            if output_path is not None:
+                save_dir_path = check_create_directory_path(dir_path=output_path)
+            else:
+                save_dir_path = dir_path
+
             fmu.save_fmu_copy(file_name=f"{fmu_file.stem}{output_suffix}", tar_dir_path=save_dir_path)
 
 
 if __name__ == "__main__":
-    reduce_fmu_model_descriptions_in_directory(dir_path="./", output_suffix=None)
+    fmu_dir = "D:/01_Git/01_BaSys4FluidSim/aas_generation/data/files/fmus"
+    reduce_fmu_model_descriptions_in_directory(dir_path=fmu_dir, output_path=None, output_suffix=None)
